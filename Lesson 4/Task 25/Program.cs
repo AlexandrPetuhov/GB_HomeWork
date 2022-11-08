f5f9cc34a5db0af8547ddf1bfe9c1ca072a491cf
Alexandr <Nefilim228@gmail.com>
2022-11-08 22:40:58 +0500

    Выполнено домашнее задание с 4-го семинара

---
+++ Lesson 4/Task 25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+int naturalDegree = 1;
+
+while (count < numB)
+{
+    naturalDegree = naturalDegree*numA;
+    count++;
+}
+
+Console.WriteLine($"Число {numA} в степени {numB} = {naturalDegree}");
---
+++ Lesson 4/Task 29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] GenerateArray(int size)
+{
+    int[] result = new int[size];
+    int i = 0;
+
+    while (i < size)
+    {
+        result[i] = new Random().Next(0, 1000);
+        i++;
+    }
+
+    return result;
+}
+
+Console.Clear();
+
+int[] array = GenerateArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
