b73297420b1fc09c78b5bc5d167344bfb9bf0a48
Alexandr <Nefilim228@gmail.com>
2022-11-14 20:53:41 +0500

    Выполнены задачи с 5 семинара

---
+++ Lesson 5/Task 34/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end);
+    }
+    return RandomArray;
+}
+
+int[] myArray = CreateRandomArray(10, 100, 1000);
+Console.WriteLine($"[{String.Join(", ", myArray)}]");
+
+int sum = 0;
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] % 2 == 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine($"Количество четных чисел в массиве = {sum}");
---
+++ Lesson 5/Task 36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end);
+    }
+    return RandomArray;
+}
+
+int[] myArray = CreateRandomArray(10, -100, 100);
+Console.WriteLine($"[{String.Join(", ", myArray)}]");
+
+int sum = 0;
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        sum += myArray[i];
+    }
+}
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях = {sum}");
